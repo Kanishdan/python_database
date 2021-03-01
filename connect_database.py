75fb416b394c035e9bc34274e49c2e034df89acb
Yogarasa kanishdan <31471087+Kanishdan@users.noreply.github.com>
2021-03-01 13:21:38 +0530

    Update connect_database.py

--- connect_database.py
+++ connect_database.py
@@ -1,1 +1,9 @@
+import mysql.connector
 
+mydb = mysql.connector.connect(host="localhost", port="3307", user="root", passwd="6961371", database="test")
+
+mycursor = mydb.cursor()
+mycursor.execute("select * from user")
+result = mycursor.fetchall()
+for i in result:
+      print(i)
